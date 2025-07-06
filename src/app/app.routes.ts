09bc36879bfe31433750576c491ba4b2f48edd43
Saulo Poveda Montesdeoca <montesdeoquin@gmail.com>
2025-07-06 04:12:55 +0200

    [ Inicio ] » Hemos creado el componente, lo hemos maquetado y lo hemos incluido en el fichero de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Components
+import { Inicio } from './components/inicio/inicio';
+
+export const routes: Routes =
+[
+    { path: '', component: Inicio }
+];
